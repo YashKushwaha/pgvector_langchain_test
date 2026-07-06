4d66c2200e8d9797a2864f7f73655dbf171ba5b9
Yash Kushwaha <37941798+YashKushwaha@users.noreply.github.com>
2026-07-06 21:45:23 +0530

    Test script for pg vector

---
+++ connectivity_check.py
@@ -0,0 +1,19 @@
+# @title Set your values or use the defaults to connect to Docker { display-mode: "form" }
+POSTGRES_USER = "postgres"  # @param {type: "string"}
+POSTGRES_PASSWORD = "mysecretpassword"  # @param {type: "string"}
+POSTGRES_HOST = "localhost"  # @param {type: "string"}
+POSTGRES_PORT = "5433"  # @param {type: "string"}
+POSTGRES_DB = "postgres"  # @param {type: "string"}
+TABLE_NAME = "vectorstore"  # @param {type: "string"}
+VECTOR_SIZE = 1024  # @param {type: "int"}
+
+# See docker command above to launch a Postgres instance with pgvector enabled.
+CONNECTION_STRING = (
+    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}"
+    f":{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+# To use psycopg3 driver, set your connection string to `postgresql+psycopg://`
+
+from langchain_postgres import PGEngine
+
+pg_engine = PGEngine.from_connection_string(url=CONNECTION_STRING)
