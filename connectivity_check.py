bcf3627efae76a52e1c73a6919fcb6655c05b8c1
Yash Kushwaha <37941798+YashKushwaha@users.noreply.github.com>
2026-07-06 22:12:46 +0530

    Basic vector search works

--- connectivity_check.py
+++ connectivity_check.py
@@ -1,3 +1,5 @@
+## https://docs.langchain.com/oss/python/integrations/vectorstores/pgvectorstore
+
 # @title Set your values or use the defaults to connect to Docker { display-mode: "form" }
 POSTGRES_USER = "postgres"  # @param {type: "string"}
 POSTGRES_PASSWORD = "mysecretpassword"  # @param {type: "string"}
@@ -6,6 +8,8 @@ POSTGRES_PORT = "5433"  # @param {type: "string"}
 POSTGRES_DB = "postgres"  # @param {type: "string"}
 TABLE_NAME = "vectorstore"  # @param {type: "string"}
 VECTOR_SIZE = 1024  # @param {type: "int"}
+
+SCHEMA_NAME="my_schema"
 
 # See docker command above to launch a Postgres instance with pgvector enabled.
 CONNECTION_STRING = (
@@ -24,3 +28,66 @@ engine = create_async_engine(
 )
 
 pg_engine = PGEngine.from_engine(engine=engine)
+
+import asyncio
+from langchain_core.embeddings import DeterministicFakeEmbedding
+
+embeddings = DeterministicFakeEmbedding(size=768)
+
+from langchain_postgres import PGVectorStore
+
+import uuid
+
+from langchain_core.documents import Document
+
+docs = [
+    Document(
+        id=str(uuid.uuid4()),
+        page_content="Red Apple",
+        metadata={"description": "red", "content": "1", "category": "fruit"},
+    ),
+    Document(
+        id=str(uuid.uuid4()),
+        page_content="Banana Cavendish",
+        metadata={"description": "yellow", "content": "2", "category": "fruit"},
+    ),
+    Document(
+        id=str(uuid.uuid4()),
+        page_content="Orange Navel",
+        metadata={"description": "orange", "content": "3", "category": "fruit"},
+    ),
+]
+
+
+
+async def main():
+    # x = await pg_engine.ainit_vectorstore_table(
+    #     table_name=TABLE_NAME,
+    #     vector_size=768,
+    #     #schema_name=SCHEMA_NAME,    # Default: "public"
+    # )
+
+    # print(x)
+
+    store = await PGVectorStore.create(
+        engine=pg_engine,
+        table_name=TABLE_NAME,
+        # schema_name=SCHEMA_NAME,
+        embedding_service=embeddings,
+    )
+
+    y = await store.aadd_documents(docs)
+    print(y)
+
+    query = "I'd like a fruit."
+    result = await store.asimilarity_search(query)
+    print('Results for query:', query)
+    print(result)
+    print()
+    query_vector = embeddings.embed_query(query)
+    result = await store.asimilarity_search_by_vector(query_vector, k=2)
+    print('Results for query:', query)
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
